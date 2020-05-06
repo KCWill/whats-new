ab954f528aa5d62c4c3726736a62c959ba57eb27
Kyle Williams <kylecharleswilliams@gmail.com>
2020-05-05 20:02:46 -0600

    Add menu functionality

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -12,26 +12,31 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      local: [local],
-      entertainment: [entertainment],
-      health: [health],
-      science: [science],
-      technology: [technology]
+      local: local,
+      entertainment: entertainment,
+      health: health,
+      science: science,
+      technology: technology,
+      category: 'local'
     }
   }
 
+  categorySelect = (category) => {
+    const categorizedNews = this.state[category];
+    this.setState({[category]: categorizedNews, category:category});
+  }
 
   render () {
     return (
       <div className="app">
         <section className='topBar'>
-          What's new?
+          <h1>What's new?</h1>
         </section>
         <section>
-        <Menu></Menu>
+        <Menu categorySelect={this.categorySelect}/>
         </section>
         <section>
-        <NewsContainer articles={local}></NewsContainer>
+        <NewsContainer articles={this.state} category={this.state.category}></NewsContainer>
         </section>
      
       </div>
--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,18 +1,23 @@
 import React from 'react';
 import './Menu.css'
 
-const Menu = () => {
-
+const Menu = (props) => {
+    
+    const handleSelection = (event) => {
+        event.preventDefault();
+        const selectedCategory = event.target.name;
+        props.categorySelect(selectedCategory);
+    };
 
     return (
-        <section className='menuContainer'>
-            <button className='menu'>Technology</button>
-            <button className='menu'>Local News</button>
-            <button className='menu'>Entertainment</button>
-            <button className='menu'>Science</button>
-            <button className='menu'>Health</button>
-        </section>
-    )
+    <section className='menuContainer'>
+        <button className='menu' name='local' onClick={handleSelection}>Local News</button>
+        <button className='menu' name='technology' onClick={handleSelection}>Technology</button>
+        <button className='menu' name='entertainment' onClick={handleSelection}>Entertainment</button>
+        <button className='menu' name='science' onClick={handleSelection}>Science</button>
+        <button className='menu' name='health' onClick={handleSelection}>Health</button>
+    </section>
+    );
 }
 
 export default Menu;
