29e9ab0737d7c17496d5e75b9e24642cb356b45d
Kyle Williams <kylecharleswilliams@gmail.com>
2020-05-05 19:03:06 -0600

    Initial commit

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -2,8 +2,15 @@ import React from 'react';
 import './Menu.css'
 
 const Menu = () => {
+
+
     return (
-        <section className='menuContainer'>  
+        <section className='menuContainer'>
+            <button className='menu'>Technology</button>
+            <button className='menu'>Local News</button>
+            <button className='menu'>Entertainment</button>
+            <button className='menu'>Science</button>
+            <button className='menu'>Health</button>
         </section>
     )
 }
